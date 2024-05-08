2972c9631d2388e89790c893dddb58bbcb4865c4
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2024-05-08 14:18:42 -0400

    sending email

--- email.js
+++ email.js
@@ -1,26 +1,37 @@
-const nodemailer = require('nodemailer');
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+
+dotenv.config();
+
+const mailHost = process.env.MAIL_HOST;
+const mailPort = process.env.MAIL_PORT;
+const mailUsername = process.env.MAIL_USERNAME;
+const mailPassword = process.env.MAIL_PASSWORD;
+
+const mailFrom = process.env.MAIL_FROM;
+const mailTo = process.env.MAIL_TO;
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: 'tucorreo@gmail.com',
-        pass: 'tucontraseña'
-    }
+  host: mailHost,
+  port: mailPort,
+  auth: {
+    user: mailUsername,
+    pass: mailPassword,
+  },
 });
 
 const mailOptions = {
-    from: '"Tu nombre" <tucorreo@gmail.com>',
-    to: 'destinatario@correo.com',
-    subject: 'Asunto del correo electrónico',
-    text: 'Cuerpo del mensaje en texto plano',
-    html: 'Cuerpo del mensaje en HTML'
+  from: mailFrom,
+  to: mailTo,
+  subject: "Asunto del correo electrónico",
+  text: "Cuerpo del mensaje en texto plano",
+  html: "Cuerpo del mensaje en HTML",
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Correo electrónico enviado: ' + info.response);
-    }
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email Send: " + info.response);
+  }
 });
