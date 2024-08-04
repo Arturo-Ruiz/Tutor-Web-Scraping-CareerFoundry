1d3f98cc0f9def4e3dda8a7d0f0c4f1698330d01
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2024-08-03 22:19:23 -0400

    chore: Add test-email script to package.json and implement email sending test functionality

---
+++ testEmail.js
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+
+dotenv.config();
+
+const mailHost = process.env.MAIL_HOST;
+const mailPort = process.env.MAIL_PORT;
+const mailUsername = process.env.MAIL_USERNAME;
+const mailPassword = process.env.MAIL_PASSWORD;
+
+const mailFrom = process.env.MAIL_FROM;
+const mailTo = process.env.MAIL_TO;
+
+const transporter = nodemailer.createTransport({
+  host: mailHost,
+  port: mailPort,
+  auth: {
+    user: mailUsername,
+    pass: mailPassword,
+  },
+});
+
+function sendEmail(subject, text) {
+  const mailOptions = {
+    from: mailFrom,
+    to: mailTo,
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email Send: " + info.response);
+    }
+  });
+}
+
+sendEmail("Test Email CareerFoundry", "This is a test email from Node.js using Nodemailer to notificate to task of CareerFoundry");
